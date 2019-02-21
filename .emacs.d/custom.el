dfdcf308cd1addef1bcb0c47ddcadc5aacfa7a34
Tony Ampomah <tampomah@emporium.co.uk>
2019-02-21 06:11:28 +0000

    Group keybindings

--- .emacs.d/custom.el
+++ .emacs.d/custom.el
@@ -3,6 +3,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(ledger-clear-whole-transactions t t)
  '(package-selected-packages
    (quote
     (yaml-mode ibuffer-projectile mu4e-alert flycheck-ledger ledger-mode atomic-chrome engine-mode wgrep helpful markdown-mode synosaurus dictionary graphviz-dot-mode gnuplot ox-pandoc org-bullets org-evil multi-term exec-path-from-shell phpactor php-extras company-php phpunit php-mode which-key smex flx counsel swiper yasnippet-snippets yasnippet autopair flycheck dired-open dired-hide-dotfiles helm-make helm-projectile projectile evil-indent-textobject evil-commentary evil-easymotion evil-tabs evil-surround evil-leader evil-collection evil-magit company ag spaceline-all-the-icons spaceline highlight-symbol all-the-icons-dired doom-themes dockerfile-mode docker tiny-menu evil use-package))))
