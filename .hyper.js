21713151aa46fb9b3b9e2addf2b9a99096e2b0e8
Tony Ampomah <tampomah@emporium.co.uk>
2019-07-17 20:25:02 +0100

    Update hyper js theme and font size

--- .hyper.js
+++ .hyper.js
@@ -1,7 +1,7 @@
 module.exports = {
   config: {
     updateChannel: 'stable',
-    fontSize: 16,
+    fontSize: 18,
     fontFamily: 'Dank Mono, "DejaVu Sans Mono", Consolas, "Lucida Console", monospace',
     fontWeight: 'normal',
     fontWeightBold: 'bold',
@@ -45,7 +45,7 @@ module.exports = {
     defaultSSHApp: true,
   },
   plugins: [
-    'hyperterm-material'
+    'hyper-one-dark-vivid'
   ],
   localPlugins: [],
   keymaps: {
