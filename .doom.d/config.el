d3a165af34cde71ab49517eeac358e4070e977ac
Tony Ampomah <tampomah@emporium.co.uk>
2019-08-04 22:09:27 +0100

    Increased line padding

--- .doom.d/config.el
+++ .doom.d/config.el
@@ -4,6 +4,23 @@
 (blink-cursor-mode 1)
 (add-to-list 'default-frame-alist '(inhibit-double-buffering . t))
 
+(defvar line-padding 3)
+(defun add-line-padding ()
+  (let ((overlays (overlays-at (point-min))))
+    (while overlays
+      (let ((overlay (car overlays)))
+        (if (overlay-get overlay 'is-padding-overlay)
+            (delete-overlay overlay)))
+      (setq overlays (cdr overlays))))
+
+  (let ((padding-overlay (make-overlay (point-min) (point-max))))
+    (overlay-put padding-overlay 'is-padding-overlay t)
+    (overlay-put padding-overlay 'line-spacing (* .1 line-padding))
+    (overlay-put padding-overlay 'line-height (+ 1 (* .1 line-padding))))
+  (setq mark-active nil))
+
+(add-hook 'buffer-list-update-hook 'add-line-padding)
+
 (setq user-full-name "Tony Ampomah"
       user-mail-address "tony@arksolutions.it"
 
