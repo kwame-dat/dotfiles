342cf69048f8b22593f44b27d6cdfc562a376d5c
Tony Ampomah <tony@arksolutions.it>
2020-03-19 23:56:01 +0000

    Update lsp configuration for performance

--- .emacs.d/lisp/init-lsp.el
+++ .emacs.d/lisp/init-lsp.el
@@ -2,39 +2,43 @@
 ;;; Commentary:
 ;; Company to setup completion
 (use-package lsp-mode
-  :init
-  (setq lsp-idle-delay 0.0)
-  (setq lsp-auto-guess-root nil)
-  (setq lsp-prefer-flymake nil)
-  (setq lsp-keep-workspace-alive nil)
-  (setq lsp-flycheck-live-reporting t)
-  (setq lsp-file-watch-threshold nil)
-  :hook (php-mode . lsp)
-  :commands lsp)
-
-(use-package company-lsp
-  :init
-  :commands company-lsp
-  :ensure t)
+  :defer t
+  :commands lsp
+  :custom
+  (lsp-auto-guess-root nil)
+  (lsp-prefer-flymake nil)
+  (lsp-file-watch-threshold 3000)
+  (read-process-output-max (* 1024 1024))
+  :bind (:map lsp-mode-map ("C-c C-f" . lsp-format-buffer))
+  :hook ((java-mode python-mode go-mode
+          js-mode js2-mode typescript-mode web-mode php-mode
+          c-mode c++-mode objc-mode) . lsp))
 
 (use-package lsp-ui
-  :init
-  (setq lsp-ui-doc-max-height 8
-        lsp-ui-doc-max-width 35
-        lsp-ui-sideline-ignore-duplicate t
-        lsp-ui-sideline-show-hover nil)
+  :after lsp-mode
+  :diminish
+  :commands lsp-ui-mode
+  :custom-face
+  (lsp-ui-doc-background ((t (:background nil))))
+  (lsp-ui-doc-header ((t (:inherit (font-lock-string-face italic)))))
+  :bind (:map lsp-ui-mode-map
+              ([remap xref-find-definitions] . lsp-ui-peek-find-definitions)
+              ([remap xref-find-references] . lsp-ui-peek-find-references)
+              ("C-c u" . lsp-ui-imenu)
+              ("M-i" . lsp-ui-doc-focus-frame))
   :custom
-  (lsp-ui-doc-enable t)
   (lsp-ui-doc-header t)
   (lsp-ui-doc-include-signature t)
-  (lsp-ui-doc-position 'top)
+  (lsp-ui-doc-enable nil)
   (lsp-ui-doc-border (face-foreground 'default))
   (lsp-ui-sideline-enable nil)
   (lsp-ui-sideline-ignore-duplicate t)
-  (lsp-ui-sideline-show-code-actions nil))
-
-(use-package lsp-ivy
-  :commands lsp-ivy-workspace-symbol lsp-ivy-global-workspace-symbol)
+  (lsp-ui-sideline-show-code-actions nil)
+  :config
+  ;; WORKAROUND Hide mode-line of the lsp-ui-imenu buffer
+  ;; https://github.com/emacs-lsp/lsp-ui/issues/243
+  (defadvice lsp-ui-imenu (after hide-lsp-ui-imenu-mode-line activate)
+    (setq mode-line-format nil)))
 
 (provide 'init-lsp)
 ;;; init-lsp.el ends here
