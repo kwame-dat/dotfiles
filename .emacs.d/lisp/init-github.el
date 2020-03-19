f53382dbbc14bb0ef9b8548c554594fe70b4779c
Tony Ampomah <tony@arksolutions.it>
2020-03-19 23:59:04 +0000

    Add private github forge url

--- .emacs.d/lisp/init-github.el
+++ .emacs.d/lisp/init-github.el
@@ -1,7 +1,6 @@
 ;;; init-github.el --- Github integration -*- lexical-binding: t -*-
 ;;; Commentary:
 ;;; Code:
-
 (require 'init-git)
 
 (use-package yagist)
@@ -9,6 +8,9 @@
 (use-package github-clone)
 (use-package forge)
 (use-package github-review)
+(push '("github.thetrainline.com" "github.thetrainline.com/api/v3"
+        "github.thetrainline.com" forge-github-repository)
+        forge-alist)
 
 (provide 'init-github)
 ;;; init-github.el ends here
