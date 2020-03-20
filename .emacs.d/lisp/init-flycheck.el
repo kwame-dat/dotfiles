c5aad6ea1f0200700d9ad39fc8de563fa8520e66
Tony Ampomah <tony@arksolutions.it>
2020-03-20 00:00:06 +0000

    Improve flycheck validations

--- .emacs.d/lisp/init-flycheck.el
+++ .emacs.d/lisp/init-flycheck.el
@@ -1,19 +1,25 @@
 ;;; init-flycheck.el --- Initialize Flycheck
 ;;; Commentary:
 ;;; Code:
-(use-package let-alist)
+(setq flycheck-display-errors-function #'flycheck-display-error-messages-unless-error-list)
+(setq flycheck-emacs-lisp-load-path 'inherit
+      flycheck-check-syntax-automatically '(save idle-change mode-enabled)
+      flycheck-idle-change-delay 0.4
+      flycheck-disabled-checkers '(php-phpmd))
 
+(setq flycheck-global-modes '(not org-mode))
+(setq flycheck-phpcs-standard "PSR2")
+(use-package let-alist)
+(use-package flycheck)
+(global-flycheck-mode)
 (use-package flycheck-phpstan)
 
-(use-package flycheck
-  :init
-  (setq flycheck-php-cs-standard "PSR2")
-  (setq flycheck-display-errors-function #'flycheck-display-error-messages-unless-error-list)
-  (setq flycheck-emacs-lisp-load-path 'inherit
-        flycheck-check-syntax-automatically '(save idle-change mode-enabled)
-        flycheck-idle-change-delay 0.4
-        flycheck-disabled-checkers '(php-phpmd)))
-(global-flycheck-mode)
+(defun my-php-mode-setup ()
+  "My PHP-mode hook."
+  (require 'flycheck-phpstan)
+  (flycheck-mode t))
+
+(add-hook 'php-mode-hook 'my-php-mode-setup)
 
 (provide 'init-flycheck)
 ;;; init-flycheck.el ends here
