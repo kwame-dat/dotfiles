38d987512a493fd4f7f551393fb33c733b5f0bb1
Tony Ampomah <tony@arksolutions.it>
2020-03-20 07:42:30 +0000

    Update email config

--- .emacs.d/lisp/init-emails.el
+++ .emacs.d/lisp/init-emails.el
@@ -2,105 +2,121 @@
 ;;; Commentary:
 ;;; Code:
 (add-to-list 'load-path "/usr/local/share/emacs/site-lisp/mu/mu4e")
-(use-package evil-mu4e)
-(use-package mu4e-alert)
-(mu4e-alert-set-default-style 'libnotify)
-(add-hook 'after-init-hook #'mu4e-alert-enable-notifications)
-
-(setq mu4e-maildir "~/.mail")
-
-(setq mu4e-headers-include-related nil)
-
-(setq mu4e-confirm-quit nil)
-
-(setq mu4e-compose-context-policy 'pick-first)
-
-(setq mail-user-agent 'mu4e-user-agent)
-
-(add-hook 'message-mode-hook 'turn-on-orgtbl)
-(add-hook 'message-mode-hook 'turn-on-orgstruct++)
-
-(setq message-kill-buffer-on-exit t)
-
-(setq mu4e-view-show-addresses t)
-
-(setq mu4e-attachment-dir "~/Downloads")
-
-(define-key mu4e-view-mode-map (kbd "C-c C-o") 'mu4e~view-browse-url-from-binding)
-
-(require 'mu4e-contrib)
-(setq mu4e-html2text-command 'mu4e-shr2text
-      shr-color-visible-luminance-min 60
-      shr-color-visible-distance-min 5
-      shr-use-fonts nil
-      shr-use-colors nil)
-(advice-add #'shr-colorize-region
-            :around (defun shr-no-colourise-region (&rest ignore)))
-
-(add-to-list 'mu4e-view-actions '("ViewInBrowser" . mu4e-action-view-in-browser) t)
-
-(require 'starttls)
+(require 'mu4e)
 (require 'smtpmail)
 
-(use-package smtpmail-multi)
-(setq smtpmail-multi-accounts
-      (quote
-       ((personal . ("tony@arksolutions.it"
-                     "smtp.zoho.com"
-                     587
-                     "tony@arksolutions.it"
-                     nil nil nil nil))
-        (work . ("tony.ampomah@netsells.co.uk"
-                 "smtp.gmail.com"
-                 587
-                 "tony.ampomah@netsells.co.uk"
-                 starttls
-                 nil nil nil)))))
+(setq mu4e-get-mail-command "offlineimap"
+      mu4e-maildir (expand-file-name "~/.mail")
+      mu4e-update-interval 180
+      message-kill-buffer-on-exit t
+      mu4e-headers-skip-duplicates t
+      mu4e-compose-signature-auto-include nil
+      mu4e-view-show-images t
+      mu4e-view-show-addresses t
+      mu4e-attachment-dir "~/Downloads"
+      mu4e-use-fancy-chars t
+      mu4e-headers-auto-update t
+      ;; message-signature-file "~/.emacs.d/.signature"
+      mu4e-compose-signature-auto-include nil
+      mu4e-view-prefer-html t
+      mu4e-compose-in-new-frame t
+      mu4e-change-filenames-when-moving t
+      message-send-mail-function 'smtpmail-send-it
+      starttls-use-gnutls t
+      smtpmail-stream-type 'starttls
+      ;;mu4e-html2text-command "w3m -T text/html"
+      )
 
-(setq smtpmail-multi-associations
-      (quote
-       (("tony.ampomah@netsells.co.uk" work)
-        ("tony@arksolutions.co.uk" personal))))
-
-(setq smtpmail-multi-default-account (quote work))
-(setq message-send-mail-function 'smtpmail-multi-send-it)
-
-(setq smtpmail-debug-info t)
-(setq smtpmail-debug-verbose t)
-
+(setq mu4e-context-policy 'pick-first)
+(setq mu4e-compose-context-policy 'always-ask)
 (setq mu4e-contexts
-      `( ,(make-mu4e-context
-           :name "Netsells"
-           :match-func (lambda (msg) (when msg
-                                       (string-prefix-p "/netsells.co.uk" (mu4e-message-field msg :maildir))))
-           :vars '(
-                   (mu4e-trash-folder . "/netsells.co.uk/[Gmail].Bin")
-                   (mu4e-drafts-folder . "/netsells.co.uk/[Gmail].Drafts")
-                   (mu4e-sent-folder . "/netsells.co.uk/[Gmail].Sent Mail")
-                   (mu4e-refile-folder . "/netsells.co.uk/[Gmail].4Archives")
-                   ))
-         ,(make-mu4e-context
-           :name "Arksolutions"
-           :match-func (lambda (msg) (when msg
-                                       (string-prefix-p "/artsolutions.it" (mu4e-message-field msg :maildir))))
-           :vars '(
-                   (mu4e-trash-folder . "/arksolutions.it/Trash")
-                   (mu4e-sent-folder . "/arksolutions.it/Sent")
-                   (mu4e-drafts-folder ."/arksolutions.it/Drafts")
-                   (mu4e-refile-folder . "/arksolutions.it/4Archives")
-                   ))
-         ))
+      (list
+       (make-mu4e-context
+    :name "personal"
+    :enter-func (lambda () (mu4e-message "Entering personal context"))
+    :leave-func (lambda () (mu4e-message "Leaving personal context"))
+    :match-func (lambda (msg)
+              (when msg
+            (mu4e-message-contact-field-matches
+             msg '(:from :to :cc :bcc) "tony@arksolutions.it")))
+    :vars '((user-mail-address . "tony@arksolutions.it")
+        (user-full-name . "Tony Ampomah")
+        (mu4e-sent-folder . "/arksolutions.it/Sent")
+        (mu4e-drafts-folder . "/arksolutions.it/Drafts")
+        (mu4e-trash-folder . "/arksolutions.it/Trash")
+        (mu4e-refile-folder . "/arksolutions.it/Archives")
+        (smtpmail-queue-dir . "~/.email/arksolutions.it/queue/cur")
+        (smtpmail-smtp-user . "tony@arksolutions.it")
+        (smtpmail-starttls-credentials . (("tony@arksolutions.it" 587 nil nil)))
+        (smtpmail-auth-credentials . (expand-file-name "~/.authinfo.gpg"))
+        (smtpmail-default-smtp-server . "smtp.zoho.com")
+        (smtpmail-smtp-server . "smtp.zoho.com")
+        (smtpmail-smtp-service . 587)
+        (mu4e-sent-messages-behavior . sent)
+        (mu4e-maildir-shortcuts . ( ("/arksolutions.it/INBOX"    . ?i)
+                        ("/arksolutions.it/Sent"     . ?s)
+                        ("/arksolutions.it/Trash"    . ?t)
+                        ("/arksolutions.it/4Archives" . ?a)
+                        ("/arksolutions.it/Drafts"   . ?d)
+                        ))))
 
-(setq mu4e-user-mail-address-list (list "tony@arksolutions.it" "tony.ampomah@netsells.co.uk"))
+       (make-mu4e-context
+    :name "work"
+    :enter-func (lambda () (mu4e-message "Entering work context"))
+    :leave-func (lambda () (mu4e-message "Leaving work context"))
+    :match-func (lambda (msg)
+              (when msg
+            (mu4e-message-contact-field-matches
+             msg '(:from :to :cc :bcc) "tony.ampomah@netsells.co.uk")))
+    :vars '((user-mail-address . "tony.ampomah@netsells.co.uk")
+        (user-full-name . "Tony Ampomah")
+        (mu4e-sent-folder . "/netsells.co.uk/[Gmail]/Sent Mail")
+        (mu4e-drafts-folder . "/netsells.co.uk/[Gmail]/Drafts")
+        (mu4e-trash-folder . "/netsells.co.uk/[Gmail]/Trash")
+        (mu4e-refile-folder . "/netsells.co.uk/[Gmail]/Archives")
+        (smtpmail-queue-dir . "~/.email/netsells.co.uk/queue/cur")
+        (smtpmail-smtp-user . "tony.ampomah@netsells.co.uk")
+        (smtpmail-starttls-credentials . (("smtp.gmail.com" 587 nil nil)))
+        (smtpmail-auth-credentials . (expand-file-name "~/.authinfo.gpg"))
+        (smtpmail-default-smtp-server . "smtp.gmail.com")
+        (smtpmail-smtp-server . "smtp.gmail.com")
+        (smtpmail-smtp-service . 587)
+        (mu4e-sent-messages-behavior . delete)
+        (mu4e-maildir-shortcuts . ( ("/netsells.co.uk/INBOX"                        . ?i)
+                        ("/netsells.co.uk/[Gmail]/Sent Mail" . ?s)
+                        ("/netsells.co.uk/[Gmail]/Trash"            . ?t)
+                        ("/netsells.co.uk/4Archives"    . ?a)
+                        ("/netsells.co.uk/[Gmail]/Drafts"           . ?d)
+                        ))))))
 
-(setq message-kill-buffer-on-exit t)
+;;; Bookmarks
+(setq mu4e-bookmarks
+      `(
+    ("flag:unread AND NOT flag:trashed" "Unread messages" ?u)
+    ("flag:unread" "new messages" ?n)
+        ("date:today..now" "Today's messages" ?t)
+        ("date:7d..now" "Last 7 days" ?w)
+        ("mime:image/*" "Messages with images" ?p)
+        ))
 
-(setq
- mu4e-view-show-images t
- mu4e-view-image-max-width 800)
+(require 'mu4e-contrib)
+(use-package evil-mu4e)
+(use-package mu4e-alert)
+
+
+(add-to-list 'mu4e-view-actions '("ViewInBrowser" . mu4e-action-view-in-browser) t)
+(mu4e-alert-set-default-style 'growl)
+(define-key mu4e-view-mode-map (kbd "C-c C-o") 'mu4e~view-browse-url-from-binding)
+(add-hook 'after-init-hook #'mu4e-alert-enable-notifications)
+(add-hook 'message-mode-hook 'turn-on-orgtbl)
+(add-hook 'message-mode-hook 'turn-on-orgstruct++)
+(add-hook 'after-init-hook #'mu4e-alert-enable-mode-line-display)
 
 (require 'org-mu4e)
+(setq org-mu4e-convert-to-html t)
 
-(setq org-mu4e-link-query-in-headers-mode nil)
+(use-package org-mime
+  :ensure t)
+
 (provide 'init-emails)
 ;;; init-emails.el ends here
