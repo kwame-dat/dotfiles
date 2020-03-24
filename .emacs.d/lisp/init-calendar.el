9d27d3107ea5ff07056c4803bd10dd913447397a
Tony Ampomah <tony@arksolutions.it>
2020-03-24 22:25:56 +0000

    Adds calendar functionality

---
+++ .emacs.d/lisp/init-calendar.el
@@ -0,0 +1,10 @@
+;;; init-calendar.el --- View my calendar in Emacs why not -*- lexical-binding: t -*-
+;;; Commentary:
+;;; Code:
+(use-package calfw)
+(use-package calfw-org)
+(use-package calfw-cal)
+(use-package calfw-ical)
+
+(provide 'init-calendar)
+;;; init-calendar.el ends here
