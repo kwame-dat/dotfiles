54292b3039a01046e34031ba2c5c23a3bfa9d653
Tony Ampomah <tony@arksolutions.it>
2020-03-30 01:28:21 +0100

    Improve reading with visual fill column mode

--- .emacs.d/lisp/init-nov.el
+++ .emacs.d/lisp/init-nov.el
@@ -2,7 +2,12 @@
 ;;; Commentary:
 ;;; Code:
 ;; Epub Reading Books
+(setq nov-text-width t)
+(setq visual-fill-column-center-text t)
 (use-package nov)
+(use-package visual-fill-column)
+(add-hook 'nov-mode-hook 'visual-line-mode)
+(add-hook 'nov-mode-hook 'visual-fill-column-mode)
 (add-to-list 'auto-mode-alist '("\\.epub\\'" . nov-mode))
 (add-to-list 'auto-mode-alist '("\\.jwpub\\'" . nov-mode))
 
