8538bf0723c61ad9803815edf63a2843bbb5bf49
Tony Ampomah <tony@arksolutions.it>
2020-06-02 22:52:57 +0100

    Improve polybar configuration

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -1,4 +1,6 @@
-#!/bin/sh
+#!/usr/bin/env sh
+
+## Add this to your wm startup file.
 
 # Terminate already running bar instances
 killall -q polybar
@@ -7,7 +9,5 @@ killall -q polybar
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
 # Launch bar1 and bar2
-polybar mainbar-bspwm;
-
-echo "Bars launched..."
-
+polybar -c ~/.config/polybar/config-t2.ini main &
+polybar -c ~/.config/polybar/config-t2.ini bottom &
