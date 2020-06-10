35e1e92225a50bea9be2c17f1925e27b2c8eccb6
Tony Ampomah <tony@arksolutions.it>
2020-06-10 12:31:57 +0100

    Revert "Delete doom configuration"
    This reverts commit 4deb4a191b672fd5b5d83881f0000e9b9cfc65a1.

---
+++ .doom.d/packages.el
@@ -0,0 +1,14 @@
+;;; ~/.config/doom/packages.el
+
+(package! apache-mode)
+(package! mu4e-alert)
+(package! nginx-mode)
+(package! org-jira)
+(package! org-pomodoro)
+(package! org-super-agenda)
+(package! ox-slack)
+(package! ox-twbs)
+(package! slack)
+(package! speed-type)
+(package! uuidgen)
+(package! phpactor)
