6963f86dd154f2158ebed01dc455549bdacf7db2
Tony Ampomah <tony@arksolutions.it>
2020-06-19 01:31:12 +0100

    Simplify polybar

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -1,13 +1,10 @@
 #!/usr/bin/env sh
 
-## Add this to your wm startup file.
-
 # Terminate already running bar instances
 killall -q polybar
 
 # Wait until the processes have been shut down
-while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
+while pgrep -x polybar >/dev/null; do sleep 1; done
 
 # Launch bar1 and bar2
-polybar -c ~/.config/polybar/config-t2.ini top &
-polybar -c ~/.config/polybar/config-t2.ini bottom &
+polybar main &
--- .config/polybar/scripts/pub-ip.sh
+++ .config/polybar/scripts/pub-ip.sh
@@ -6,5 +6,5 @@ IP=$(dig +short myip.opendns.com @resolver1.opendns.com)
 if pgrep -x openvpn > /dev/null; then
     echo VPN: $IP
 else
-    echo   $IP
+    echo $IP
 fi
