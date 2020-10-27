4ad61df0bbe238f2533411a77bd9630c638c6136
Tony Ampomah <tony@arksolutions.it>
2020-10-27 01:08:13 +0000

    Update scripts & picom config

--- .xmonad/scripts/autostart.sh
+++ .xmonad/scripts/autostart.sh
@@ -28,11 +28,10 @@ run pamac-tray &
 run xfce4-power-manager &
 numlockx on &
 blueberry-tray &
-picom --config $HOME/.xmonad/scripts/picom.conf &
+picom &
 /usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 &
 /usr/lib/xfce4/notifyd/xfce4-notifyd &
 /usr/bin/emacs --daemon &
-
 # dunst &
 
 #starting user applications at boot time
