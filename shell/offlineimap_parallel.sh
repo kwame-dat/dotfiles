e38907217fa27fbd31cc1b4982936aa649ff7934
Tony Ampomah <tony@arksolutions.it>
2020-11-24 21:32:25 +0000

    Update dotfiles

--- shell/offlineimap_parallel.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-offlineimap -a jw & pid1=$!
-offlineimap -a arksolutions & pid2=$!
-offlineimap -a netsells & pid3=$!
-
-wait $pid1
-wait $pid2
-wait $pid3
-echo "Last execution: $(date)"
