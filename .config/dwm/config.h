d1a76c97e2db469fa80e58b2cfc7cf71b2566dfa
Tony Ampomah <tony@arksolutions.it>
2020-12-13 22:12:37 +0000

    Add dwm sticky patch

--- .config/dwm/config.h
+++ .config/dwm/config.h
@@ -128,6 +128,7 @@ static Key keys[] = {
 	{ MODKEY,                       XK_minus,  setgaps,        {.i = -1 } },
 	{ MODKEY,                       XK_equal,  setgaps,        {.i = +1 } },
 	{ MODKEY|ShiftMask,             XK_equal,  setgaps,        {.i = 0  } },
+  { MODKEY,                       XK_s,      togglesticky,   {0} },
 	{ MODKEY,                       XK_grave,  view,           {0} },
 	{ MODKEY|ShiftMask,             XK_q,      killclient,     {0} },
 	{ MODKEY,            			      XK_q,      killclient,     {0} },
