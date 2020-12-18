d2427fb19e69dbb885e5ab09702918fb0e4d535a
Tony Ampomah <tony@arksolutions.it>
2020-12-18 12:10:02 +0000

    Update config

--- .config/dwm/autostart.sh
+++ .config/dwm/autostart.sh
@@ -6,7 +6,7 @@ function run {
   fi
 }
 
-xrandr --output eDP1 --mode 3840x2160 --pos 3840x0 --rotate normal --output DP1 --primary --mode 3840x1600 --pos 0x0 --rotate normal --output DP2 --off --output DP3 --off --output VIRTUAL1 --off &
+xrandr --output eDP1 --off --output DP1 --primary --mode 3840x1600 --pos 0x0 --rotate normal --output DP2 --off --output DP3 --off --output VIRTUAL1 --off &
 
 # cursor active at boot
 xsetroot -cursor_name left_ptr &
