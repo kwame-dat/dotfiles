2e6839a2c5d32c161d7c1ae2db15be71e6175175
Tony Ampomah <tony@arksolutions.it>
2020-12-20 19:32:56 +0000

    Remove unused tray from autostart

--- .xmonad/scripts/autostart.sh
+++ .xmonad/scripts/autostart.sh
@@ -36,20 +36,15 @@ xsetroot -cursor_name left_ptr &
 #start ArcoLinux Welcome App
 run dex $HOME/.config/autostart/arcolinux-welcome-app.desktop
 
-#Some ways to set your wallpaper besides variety or nitrogen
-feh --bg-fill /usr/share/backgrounds/arcolinux/arco-wallpaper.jpg &
-
 #starting utility applications at boot time
 run variety &
 run nm-applet &
-run pamac-tray &
 run xfce4-power-manager &
-run volumeicon &
 numlockx on &
-blueberry-tray &
 picom &
 /usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 &
 /usr/lib/xfce4/notifyd/xfce4-notifyd &
+# run pamac-tray &
 
 #starting user applications at boot time
 /usr/bin/emacs --daemon &
