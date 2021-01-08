857480d30407289b734df2dd65ea20f16cc279d7
Tony Ampomah <tony@arksolutions.it>
2021-01-08 18:46:33 +0000

    Update multi monitor script

--- .bin/multi-monitor.sh
+++ .bin/multi-monitor.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-xrandr --output eDP1 --primary --mode 3840x2160 --pos 3840x0 --rotate normal --output DP1 --mode 3840x1600 --pos 0x0 --rotate normal --output DP2 --off --output DP3 --off --output VIRTUAL1 --off
+xrandr --output eDP1 --primary --mode 3840x2160 --pos 0x2166 --rotate normal --output DP1 --mode 3840x1600 --pos 0x0 --rotate normal --output DP2 --off --output DP3 --off --output VIRTUAL1 --off
