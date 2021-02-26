e40e5a7cb5bb7a6a2e2c103218a6383a2baa5a1b
Tony Ampomah <tony@arksolutions.it>
2021-02-26 20:28:19 +0000

    Remove unused scripts

--- .bin/multi-monitor.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-xrandr --output eDP1 --primary --mode 3840x2160 --pos 0x2166 --rotate normal --output DP1 --mode 3840x1600 --pos 0x0 --rotate normal --output DP2 --off --output DP3 --off --output VIRTUAL1 --off
--- .bin/single-ultrawide.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-xrandr --output eDP1 --off --output DP1 --mode 3840x1600 --pos 0x0 --rotate normal --output DP2 --off --output DP3 --off --output VIRTUAL1 --off
