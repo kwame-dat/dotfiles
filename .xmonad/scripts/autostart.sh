189e7139394b135178b710c4ac42a9481e171d6b
Tony Ampomah <tony@arksolutions.it>
2021-04-30 16:36:09 +0100

    Update xmonad autostart

--- .xmonad/scripts/autostart.sh
+++ .xmonad/scripts/autostart.sh
@@ -7,9 +7,6 @@ function run {
   fi
 }
 
-# cursor active at boot
-xsetroot -cursor_name left_ptr &
-
 # Setup my Touchpad to my liking
 xinput --set-prop "SYNA2393:00 06CB:7A13 Touchpad" "libinput Tapping Enabled" 1 &
 xinput --set-prop "SYNA2393:00 06CB:7A13 Touchpad" "libinput Natural Scrolling Enabled" 1 &
@@ -17,7 +14,7 @@ xinput --set-prop "SYNA2393:00 06CB:7A13 Touchpad" "libinput Accel Speed" 1 &
 
 # Keyboard changes
 ## make CapsLock behave like Ctrl:
-setxkbmap -option ctrl:nocaps
+setxkbmap -option ctrl:nocaps &
 
 ## make short-pressed Ctrl behave like Escape:
 xcape -e 'Control_L=Escape'
@@ -25,19 +22,22 @@ xcape -e 'Control_L=Escape'
 ## Increase keyboard key repeat
 xset r rate 300 80 &
 
-xrdb ~/.Xresources
+xrdb ~/.Xresources &
+
+# Cursor active at boot
+xsetroot -cursor_name left_ptr &
 
 # Starting utility applications at boot time
 run nm-applet &
-run picom &
 run dunst &
 /usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 &
+run xmodmap ~/.Xmodmap &
+run xbindkeys -f ~/.xbindkeysrc &
 run variety &
-xmodmap ~/.Xmodmap &
+run picom &
 
 # starting user applications at boot time
 run /usr/bin/emacs --daemon &
 run nextcloud &
 run kdeconnect-indicator &
-run xbindkeys -f ~/.xbindkeysrc &
 run redshift &
